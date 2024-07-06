0ca7fedfc619d9c931c33079b0c3bd505c94ec8b
Maximillian Leonard <maximillian1508@gmail.com>
2024-07-06 23:09:06 +0800

    refactor: migrate from eslint & prettier to biome

--- config/databaseSetup.js
+++ config/databaseSetup.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const DB_URL = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PWD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}`;
 
+// const DB_URL = process.env.MONGO_URL;
+
 const connection = async () => {
-    try {
-        await mongoose.connect(DB_URL);
-        console.log('Database is connected');
-    } catch (error) {
-        console.log(error);
-    }
+	try {
+		await mongoose.connect(DB_URL, { dbName: "easyrent" });
+		console.log("Database is connected");
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 module.exports = connection;
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,44 +1,42 @@
-/* eslint-disable no-param-reassign */
 const handleDuplicateKeyError = (err, res) => {
-    const field = Object.keys(err.keyValue);
-    const newArr = [];
+	const field = Object.keys(err.keyValue);
+	const newArr = [];
 
-    const code = 409;
-    const error = `An account with that ${field} already exists.`;
-    newArr.push({ [field]: error });
+	const code = 409;
+	const error = `An account with that ${field} already exists.`;
+	newArr.push({ [field]: error });
 
-    err = newArr;
-    res.status(code).send(err);
+	err = newArr;
+	res.status(code).send(err);
 };
 
 const handleValidationError = (err, res) => {
-    const fields = Object.keys(err.errors);
-    const newErr = [];
+	const fields = Object.keys(err.errors);
+	const newErr = [];
 
-    for (let i = 0; i < fields.length; i++) {
-        const property = Object.values(err.errors[fields[i]]);
-        const field = property[0].path;
-        const { message } = property[0];
-        newErr.push({ [field]: message });
-    }
-    err = newErr;
-    res.status(400).json(err);
+	for (let i = 0; i < fields.length; i++) {
+		const property = Object.values(err.errors[fields[i]]);
+		const field = property[0].path;
+		const { message } = property[0];
+		newErr.push({ [field]: message });
+	}
+	err = newErr;
+	res.status(400).json(err);
 };
 
-// eslint-disable-next-line consistent-return
 const errorHandler = (err, req, res, next) => {
-    try {
-        if (err.name === 'ValidationError') {
-            err = handleValidationError(err, res);
-            return next(err);
-        } if (err.code && err.code === 11000) {
-            err = handleDuplicateKeyError(err, res);
-            return next(err);
-        }
-    // eslint-disable-next-line no-shadow
-    } catch (err) {
-        return res.status(500).send('An unknown error occurred.');
-    }
+	try {
+		if (err.name === "ValidationError") {
+			err = handleValidationError(err, res);
+			return next(err);
+		}
+		if (err.code && err.code === 11000) {
+			err = handleDuplicateKeyError(err, res);
+			return next(err);
+		}
+	} catch (err) {
+		return res.status(500).send("An unknown error occurred.");
+	}
 };
 
 module.exports = errorHandler;
